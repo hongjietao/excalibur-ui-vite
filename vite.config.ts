301348274c0e0df2045c3c03dc3d738f903325ea
陶鸿杰 <18790315365@163.com>
2022-09-04 23:18:22 +0800

    feat: add vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
@@ -40,6 +42,18 @@ export default defineConfig({
       fileName: "excalibur-ui",
       // 导出模块格式
       formats: ["esm", "umd", "iife"],
+    },
+  },
+
+  test: {
+    // enable jest-like global test APIs
+    globals: true,
+    // simulate DOM with happy-dom
+    // (requires installing happy-dom as a peer dependency)
+    environment: "happy-dom",
+    // 支持tsx组件，很关键
+    transformMode: {
+      web: [/.[tj]sx$/],
     },
   },
 });
